9ee203fdfa96b7849b598c3609b49ee13e06442f
mhokchuekchuek <96624045+mhokchuekchuek@users.noreply.github.com>
2026-01-06 03:04:11 +0700

    fix(postgres): prevent duplicate database creation error

--- data/init_postgres.sql
+++ data/init_postgres.sql
@@ -1,5 +1,6 @@
 -- Create litellm database for LiteLLM proxy
 CREATE DATABASE litellm;
 
--- Create erp_agent database for long-term memory store
-CREATE DATABASE erp_agent;
+-- Create erp_agent database for long-term memory store (if not exists)
+-- Note: erp_agent may already be created by POSTGRES_DB env var
+SELECT 'CREATE DATABASE erp_agent' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'erp_agent')\gexec
